3a4bb7fe980ae7f8b94542c667d0745bec6a783c
Yinghai Zhao <zyinghai@gmail.com>
2017-12-09 16:12:35 -0500

    Chapter 0

---
+++ 0/2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(){
+  std::cout<< "This is a (\"), and this is an (\\)" << std::endl;
+}
---
+++ 0/5.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+int main () std::cout << 3+4 <<std::endl;
+
